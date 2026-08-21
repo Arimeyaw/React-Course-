965d5081768b5e8478b0a7516344d1024302ed8c
Arimeyaw Bartah Mohammed <122635796+Arimeyaw@users.noreply.github.com>
2026-08-21 02:42:57 +0000

    Learn rendering lists with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from "react";
 import "./App.css";
+
+function TodoList(){
+  const todos = [
+    {id: 1, text: "Learn JSX"},
+    {id: 2, text: "Understand props"},
+    {id: 3, text: "Master state & events"},
+  ];
+  return(
+    <div>
+      <h2>My React Todos</h2>
+      <ul>
+        {todos.map((todo)=>(
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function handleNameChange(event) {
-    const value = event.target.value;
-    setName(value);
-
-  }
-  function handleEmailChange(event) {
-    const value = event.target.value;
-    setEmail(value);
-  }
-  function handlePasswordChange(event) {
-    const value = event.target.value;
-    setPassword(value);
-  }
   return (
     <div>
-      <input type="text" placeholder="name..." onChange={handleNameChange} />
-      <p>{name}</p>
-      <input type="text" placeholder="email" onChange={handleEmailChange} />
-      <p>{email}</p>
-      <input
-        type="password"
-        placeholder="password"
-        onChange={handlePasswordChange}
-      />
-      <p>{password}</p>
+      <TodoList/>
     </div>
   );
 }
